4893b331b34c18197781f4c7c21de9aa8ddcec54
111 <111@qq.com>
2018-07-05 15:41:41 +0800

    添加python代码文件：pyt1

---
+++ pyt1.py
@@ -0,0 +1,46 @@
+print("zhangsan")
+
+# 注释
+
+a = 100
+
+print(a)
+
+a = 'zhangsan'
+
+print(a)
+
+print("my name is %s" %a)
+
+name = 'lisi'
+age = 30
+
+print("my age is %d" %age)
+
+print("my name is %s, \n my age is %d" %(name,age))
+
+
+userLoginName = "zhangsan"
+
+qq = "18888888"
+print("======\n 姓名：%s\n QQ:%s\n ======"%(userLoginName,qq))
+
+a=2
+b=1
+
+a,b=2,1
+
+a,b=b,a
+
+print(a)
+
+# help(print)
+
+
+# s=input("请输入姓名：")
+#
+# print(type(s))
+
+print(20**3)
+
+
